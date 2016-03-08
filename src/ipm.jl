a995d89422c3f1190dc9ded02062f37ed3710c2d
josmanps <jmps2812@gmail.com>
2016-03-08 10:11:24 -0600

    Step function and predictor step equations solved

--- src/ipm.jl
+++ src/ipm.jl
@@ -46,7 +46,31 @@ function smw_solve(d::Array{Float64,2},
 
 end
 
-    
+
+function step(x::Array{Float64,2},
+              dx::Array{Float64,2},
+              tau::Float64)
+    #=
+
+    This method compute the step Beta,
+    such that:
+        x + Beta * dx > 0
+
+    =#
+    neg = dx .< 0
+
+    if sum(neg) == 0
+        beta = tau;
+    else
+        x = x[neg]
+        dx = dx[neg]
+        dx = x ./ dx
+        beta = tau * findmin(-dx)[1]
+        beta = min(1.0, beta)
+    end
+
+    return beta
+end
 
 
 function svm_ipm_dual(X::Array{Float64,2},
@@ -80,12 +104,23 @@ function svm_ipm_dual(X::Array{Float64,2},
     # ---------------------------
     data = SVM_train_data(X, y)
     X = 0
-    T = kernel_ichol(data, kernel, tol_ichol, maxdim)
+    V = kernel_ichol(data, kernel, tol_ichol, maxdim)
     data = 0
 
     while mu > tol_ipm && iter < maxiter
 
       # Solve predictor equations
+      # --------------------------
+      C_alpha = C - alpha
+      D = s ./ alpha + xi ./ C_alpha
+      r = -1 - b * y - s + xi - C * xi ./ C_alpha
+      QDy = smw_solve(D, V, y)
+      QDr = smw_solve(D, V, r)
+
+      D_b = QDr / (y' * QDy)
+      D_alpha = -alpha + QDy * D_b - QDr
+      D_xi = (xi ./ C_alpha) .* (D_alpha - C_alpha)
+      D_s = (-s ./ alpha) .* (D_alpha - alpha)
 
       # Compute step
 
