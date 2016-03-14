ad2e037a545a1bbeebf5e5ce566a82f0cc24740b
josmanps <jmps2812@gmail.com>
2016-03-13 23:30:20 -0600

    Código completo IPM serial. Falta debuggear.

--- src/ipm.jl
+++ src/ipm.jl
@@ -94,11 +94,13 @@ function svm_ipm_dual(X::Array{Float64,2},
     iter = 1
 
     alpha = spzeros(n, 1)
-    b = 1
+    b = ones(1, 1)
     s = ones(n, 1)
     xi = ones(n, 1)
+    C_alpha = C - alpha
 
     mu = C / 2
+    tol_ipm *= 1 + mu
 
     # Kernel Matrix approximation
     # ---------------------------
@@ -109,42 +111,73 @@ function svm_ipm_dual(X::Array{Float64,2},
 
     while mu > tol_ipm && iter < maxiter
 
-      # Solve predictor equations
-      # --------------------------
-      C_alpha = C - alpha
-      D = s ./ alpha + xi ./ C_alpha
-      r = -1 - b * y - s + xi - C * xi ./ C_alpha
-      QDy = smw_solve(D, V, y)
-      QDr = smw_solve(D, V, r)
+        # Solve predictor equations
+        # --------------------------
+        D = s ./ alpha + xi ./ C_alpha
+        r = -1 - b * y - s + xi - C * xi ./ C_alpha
+        QDy = smw_solve(D, V, y)
+        QDr = smw_solve(D, V, r)
 
-      D_b = (y' * QDr) / (y' * QDy)
-      D_alpha = -alpha + QDy * D_b - QDr
-      D_xi = (xi ./ C_alpha) .* (D_alpha - C_alpha)
-      D_s = (-s ./ alpha) .* (D_alpha + alpha)
+        D_b = (y' * QDr) / (y' * QDy)
+        D_alpha = -alpha + QDy * D_b - QDr
+        D_xi = (xi ./ C_alpha) .* (D_alpha - C_alpha)
+        D_s = (-s ./ alpha) .* (D_alpha + alpha)
 
-      # Compute step
-      # -------------
-      B_alpha = step(alpha, D_alpha, 1.0)
-      B_s = step(s, D_s, 1.0)
-      B_xi = step(xi, D_xi, 1.0)
+        # Compute step
+        # -------------
+        B_alpha = step(alpha, D_alpha, 1.0)
+        B_s = step(s, D_s, 1.0)
+        B_xi = step(xi, D_xi, 1.0)
 
-      # Compute sigma
-      # --------------
-      mu_aff = (s + B_s * D_s)' * (alpha + B_alpha * D_alpha)
-      mu_aff += (C_alpha - B_alpha * D_alpha)' * (xi + B_xi * D_xi)
-      mu_aff /= 2 * n
-      mu_aff = mu_aff[1]
-      sigma = (mu_aff / mu)^3
+        # Compute sigma
+        # --------------
+        mu_aff = (s + B_s * D_s)' * (alpha + B_alpha * D_alpha)
+        mu_aff += (C_alpha - B_alpha * D_alpha)' * (xi + B_xi * D_xi)
+        mu_aff /= 2 * n
+        mu_aff = mu_aff[1]
+        sigma = (mu_aff / mu)^3
 
-      # Solve corrector equations
+        # Solve corrector equations
+        # -------------------------
+        D = s ./ (alpha + D_alpha) + xi ./ (C_alpha - D_alpha)
+        sig_mu = sigma * mu
+        alpha_D_alpha = alpha + D_alpha
+        C_alpha_D_alpha = C_alpha - D_alpha
 
-      # Compute step
+        A_sig_mu = sig_mu ./ (alpha_D_alpha)
+        CA_sig_mu = sig_mu ./ (C_alpha_D_alpha)
+        w = A_sig_mu - CA_sig_mu
+        r = -1 - b * y - s + xi - C * xi ./ (C_alpha_D_alpha)
+        u = r - w
+        QDy = smw_solve(D, V, y)
+        QDu = smw_solve(D, V, u)
 
-      # Update
+        D_b = (y' * QDu) / (y' * QDy)
+        D_alpha_2 = -alpha + QDy * D_b - QDu
+        D_xi = (xi ./ (C_alpha_D_alpha)) .* (D_alpha_2 - C_alpha) + CA_sig_mu
+        D_s = (-s ./ (alpha_D_alpha)) .* (D_alpha_2 + alpha) + A_sig_mu
+        D_alpha = D_alpha_2
+        D_alpha_2 = 0
+
+        # Compute step
+        # -------------
+        B_alpha = step(alpha, D_alpha, 0.995)
+        B_s = step(s, D_s, 0.995)
+        B_xi = step(xi, D_xi, 0.995)
+        B, ~ = findmin([B_alpha, B_s, B_xi])
+
+        # Update
+        # -------
+        alpha += B_alpha * D_alpha
+        b += B * D_b
+        s += B_s * D_s
+        xi += B_xi * D_xi
+        C_alpha = C - alpha
+        mu = (alpha' * s + C_alpha * xi)[1]
+        mu /= 2*n
 
     end    
 
-
-    return 0
+    return (alpha, b, s, xi)
 
 end
